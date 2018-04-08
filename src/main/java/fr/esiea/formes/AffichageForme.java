f492e03441e386f0b5cd56cc50d5485d40fdea29
SH <seb.frkl@hotmail.fr>
2018-04-08 18:28:21 +0200

    Added save button to play

--- src/main/java/fr/esiea/formes/AffichageForme.java
+++ src/main/java/fr/esiea/formes/AffichageForme.java
@@ -7,10 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.io.File;
 import java.io.IOException;
+import java.nio.BufferUnderflowException;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -23,51 +26,60 @@ import fr.esiea.glpoo.map.Map;
 import fr.esiea.glpoo.map.Position;
 
 public class AffichageForme {
-	 
-	public static Image getImageFromMap(Map map)
-	{
-	    BufferedImage image = new BufferedImage(map.getSizex(), map.getSizey(), BufferedImage.TYPE_INT_RGB);
-	    for(int col = 0; col < map.getSizey(); col++)
-	    {
-	    	for (int line = 0; line < map.getSizex(); line ++)
-	    	{
-	    		if (map.getNodeByPos(col, line).getColor().getAlias().equalsIgnoreCase(Couleur.blanc.alias))
-	    		{
-	    			image.setRGB(line, col, Color.YELLOW.getRGB());
-	    		}
-	    		else
-	    		{
-	    			image.setRGB(line, col, Color.BLUE.getRGB());
-	    		}
-	    	}
-	    }
-	    return image;
-	  }
-	
-	public static Map createRandomPoly()
-	{
+
+	public static Image getImageFromMap(Map map) {
+		BufferedImage image = new BufferedImage(map.getSizex(), map.getSizey(), BufferedImage.TYPE_INT_RGB);
+		for (int col = 0; col < map.getSizey(); col++) {
+			for (int line = 0; line < map.getSizex(); line++) {
+				if (map.getNodeByPos(col, line).getColor().getAlias().equalsIgnoreCase(Couleur.blanc.alias)) {
+					image.setRGB(line, col, Color.YELLOW.getRGB());
+				} else {
+					image.setRGB(line, col, Color.BLUE.getRGB());
+				}
+			}
+		}
+		return image;
+	}
+
+	public static Map createRandomPoly() {
 		CsvTirageDao csvTirage = new CsvTirageDao();
 		Tirage tirage = csvTirage.findRandomTirage();
-		Map map = new Map(500,500);
-		Position pos2 = new Position (tirage.getBoules()[0]*10,tirage.getEtoiles()[0]*10);
-		Position pos3 = new Position(tirage.getBoules()[1]*10,tirage.getEtoiles()[1]*10);
-		Position pos4 = new Position (tirage.getBoules()[2]*10, tirage.getBoules()[3]*10);
-		Position pos5 = new Position(tirage.getBoules()[4]*10,tirage.getBoules()[0]*10);
-		List<Position> ListPos= Arrays.asList(pos2,pos3,pos4,pos5);
-		
+		Map map = new Map(500, 500);
+		Position pos2 = new Position(tirage.getBoules()[0] * 10, tirage.getEtoiles()[0] * 10);
+		Position pos3 = new Position(tirage.getBoules()[1] * 10, tirage.getEtoiles()[1] * 10);
+		Position pos4 = new Position(tirage.getBoules()[2] * 10, tirage.getBoules()[3] * 10);
+		Position pos5 = new Position(tirage.getBoules()[4] * 10, tirage.getBoules()[0] * 10);
+		List<Position> ListPos = Arrays.asList(pos2, pos3, pos4, pos5);
+
 		map.addPolygone(ListPos, false);
 		return map;
 	}
 	
-	
-	public static void displayRandomPoly()
-	{
+	public static Map createRandomCircle() {
+		CsvTirageDao csvTirage = new CsvTirageDao();
+		Tirage tirage = csvTirage.findRandomTirage();
+		Map map = new Map(500, 500);
+		Position pos2 = new Position(tirage.getBoules()[0] * 10, tirage.getEtoiles()[0] * 10);
+		Position pos3 = new Position(tirage.getBoules()[1] * 10, tirage.getEtoiles()[1] * 10);
+		List<Position> ListPos = Arrays.asList(pos2, pos3);
+
+		map.addFormeSimple(pos2.getPositionx(), pos2.getPositiony(), FormeType.cercle,pos3.getPositionx() , pos3.getPositiony());
+		return map;
+	}
+
+	public static void displayRandomPoly() {
 		Map map = createRandomPoly();
 		Image image = AffichageForme.getImageFromMap(map);
 		AffichageForme.displayImage(image);
 	}
-	 
-	  public static void displayImage(Image image)
+	
+    static void sauverImage(BufferedImage image) throws IOException
+	{
+		File nomfichier = new File("./image.png");
+		ImageIO.write(image, "png", nomfichier);
+	} 
+
+	public static void displayImage(Image image)
 	  {
 		ImageIcon icon = new ImageIcon(image);
 		JFrame frame = new JFrame();
@@ -89,10 +101,7 @@ public class AffichageForme {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			    System.out.println("hey");
-
 				Map mapp = createRandomPoly();
-				System.out.println(mapp != null);
 			    img.setIcon(new ImageIcon(getImageFromMap(mapp)));
 				frame.revalidate();
 				frame.repaint();
@@ -100,23 +109,43 @@ public class AffichageForme {
 			}
 		});
 	    JButton buttonCircle = new JButton("new circle");
-	    buttonPoly.addActionListener(new ActionListener() {
+	    buttonCircle.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				Map mapp = createRandomCircle();
+				img.setIcon(new ImageIcon(getImageFromMap(mapp)));
 				frame.revalidate();
 				frame.repaint();
 				
 			}
 		});
+	    
+
+	    
+	    JButton buttonSave = new JButton("save(in root)");
+	    buttonSave.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					sauverImage((BufferedImage)image);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+
 	    img.setIcon(icon);
 	    jp.add(img);
 	    jp.add(buttonClose);
 	    jp.add(buttonCircle);
 	    jp.add(buttonPoly);
+	    jp.add(buttonSave);
 	    frame.add(jp);
 	    frame.setSize(new Dimension(1000, 1000));
 	    frame.setVisible(true);
 	  }
-	  
+
 }
